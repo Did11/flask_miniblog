05322ea2ca5ad563f23138f5865282abdf19b106
Didier Oliva <di.oliva@itecriocuarto.org.ar>
2023-11-08 11:18:47 -0300

    update init

--- app/__init__.py
+++ app/__init__.py
@@ -3,41 +3,36 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_wtf import CSRFProtect
-from werkzeug.utils import secure_filename  # Importamos esta utilidad para asegurar nombres de archivos
+from werkzeug.utils import secure_filename
 from dotenv import load_dotenv
-from flask_jwt_extended import JWTManager  # Importa JWTManager
+from flask_jwt_extended import JWTManager
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 load_dotenv()
 
 app = Flask(__name__)
 app.debug = True
-
-# Importa configuraciones de config.py
 app.config.from_object('config')
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 csrf = CSRFProtect(app)
-
-# Inicialización de Bcrypt
 bcrypt = Bcrypt(app)
+jwt = JWTManager(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
 
-# Configuración del directorio para guardar imágenes de perfil
 app.config['PROFILE_IMAGES_DEST'] = os.path.join(app.root_path, 'static/images/profile_images')
 print(app.config['PROFILE_IMAGES_DEST'])
 
-# Asegúrate de que el directorio para guardar imágenes de perfil exista
 if not os.path.exists(app.config['PROFILE_IMAGES_DEST']):
     os.makedirs(app.config['PROFILE_IMAGES_DEST'])
 
-# Inicializa Flask-JWT-Extended
-jwt = JWTManager(app)
-
-# Ahora importa y registra el Blueprint
+# Importaciones retrasadas para evitar importaciones circulares
 from .routes import users
 app.register_blueprint(users)
+
 
 from . import routes, models
 
